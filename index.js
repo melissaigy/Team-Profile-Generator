c482671422aa32a793b001001c41fe8e57dc8717
Melissa Igyarto <110946831+melissaigy@users.noreply.github.com>
2022-12-13 18:10:00 -0500

    helped html in generateHTML doc

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const inquirer = require('inquirer');
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
-const generateHTML = require('./src/generateHTML');
+const generateHTML = require('./src/generateHTML.js');
 
 const teamArray = []; 
 
